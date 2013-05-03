fb3d750cb7742c0aedc0fafe298319b0324670b5
Jeremy Baker <jhubert@gmail.com>
2013-05-03 00:20:22 -0700

    Update to version 3021

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
-# Install Sublime Text 2 into /Applications
+# Install Sublime Text 3 into /Applications
 #
 # Usage:
 #
-#     include sublime_text_3
+#   include sublime_text_3
 class sublime_text_3 {
   package { 'SublimeText3':
     provider => 'appdmg',
-    source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203012.dmg';
+    source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203021.dmg';
   }
 
   file { "${boxen::config::bindir}/subl":
